5df26f772eae96967e747c5076da09ed519a8161
kentakang <service@kentastudio.com>
2015-04-19 22:21:57 +0900

    Initial commit

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	include "kakao.config.php";
-?>
-<a href="https://kauth.kakao.com/oauth/authorize?client_id=<?=$api_key?>&redirect_uri=<?=$domain?>/kakao.login.php&response_type=code">카카오 로그인</a>
