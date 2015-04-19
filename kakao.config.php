db3b703b7fcc892410751ada58170486c16895a3
kentakang <service@kentastudio.com>
2015-04-19 21:32:02 +0900

    Add config file

---
+++ kakao.config.php
@@ -0,0 +1,7 @@
+<?php
+	
+	$api_key = ""; // 카카오 개발자 페이지에서 생성한 앱의 REST API KEY 를 입력해주세요.
+	$domain = ""; // 사이트의 도메인을 입력해주세요.
+	$token_api = "https://kauth.kakao.com/oauth/token"; // 수정하지 마세요 
+
+?>
