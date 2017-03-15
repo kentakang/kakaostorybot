e7cb0b9cbab08909396d3e305d8e1003429f4365
kentakang <kentakang@icloud.com>
2017-03-16 04:09:34 +0900

    Added New API

--- story.api.php
+++ story.api.php
@@ -1,10 +1,28 @@
 <?php
-	require_once ("kakao.config.php"); // 기본 설정 불러오기
+	require_once ("story.config.php");
 
-	$i = 1;
-
-	ini_set('max_execution_time', 0);
-
+	// 프로필 요청 api
+	function kakao_getprofile() {
+		$token = $_SESSION['token'];
+		$opts = array(
+			CURLOPT_HTTPHEADER => array(
+				"Authorization: Bearer {$token}"
+			),
+			CURLOPT_URL => "https://kapi.kakao.com/v1/api/story/profile",
+			CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_SSLVERSION => 1,
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $params,
+			CURLOPT_RETURNTRANSFER => true,
+		);
+		$curl = curl_init();
+		curl_setopt_array($curl, $opts);
+		$result = curl_exec($curl);
+		echo $result;
+		curl_close($curl);
+	}
+	
+	// 포스팅 api
 	function kakao_posting($content, $permission, $enable_share) {
 		$token = $_SESSION['token'];
 		$params = sprintf('content='.$content.'&permission='.$permission.'&enable_share='.$enable_share);
@@ -26,5 +44,68 @@
 		curl_close($curl);
 	}
 
-	kakao_posting('test', 'F', 'true');
+	// 단일 스토리 정보 요청 api
+	function kakao_getstoryinfo($id) {
+		$token = $_SESSION['token'];
+		$params = sprintf('id='.$id);
+		$opts = array( 
+			CURLOPT_HTTPHEADER => array(
+				"Authorization: Bearer {$token}"
+			),
+			CURLOPT_URL => "https://kapi.kakao.com/v1/api/story/mystory", 
+			CURLOPT_SSL_VERIFYPEER => false, 
+				CURLOPT_SSLVERSION => 1,
+			CURLOPT_POST => true, 
+			CURLOPT_POSTFIELDS => $params, 
+			CURLOPT_RETURNTRANSFER => true, 
+		);
+		$curl = curl_init();
+		curl_setopt_array($curl, $opts);
+		$result = curl_exec($curl);
+		echo $result;
+		curl_close($curl);
+	}
+	// 복수 스토리 정보 요청 api
+	function kakao_getmstoryinfo($last_id) {
+		$token = $_SESSION['token'];
+		$params = sprintf('last_id='.$last_id);
+		$opts = array( 
+			CURLOPT_HTTPHEADER => array(
+				"Authorization: Bearer {$token}"
+			),
+			CURLOPT_URL => "https://kapi.kakao.com/v1/api/story/mystories", 
+			CURLOPT_SSL_VERIFYPEER => false, 
+				CURLOPT_SSLVERSION => 1,
+			CURLOPT_POST => true, 
+			CURLOPT_POSTFIELDS => $params, 
+			CURLOPT_RETURNTRANSFER => true, 
+		);
+		$curl = curl_init();
+		curl_setopt_array($curl, $opts);
+		$result = curl_exec($curl);
+		echo $result;
+		curl_close($curl);
+	}
+
+	// 스토리 삭제 api
+	function kakao_storydelete($id) {
+		$token = $_SESSION['token'];
+		$params = sprintf('id='.$id);
+		$opts = array( 
+			CURLOPT_HTTPHEADER => array(
+				"Authorization: Bearer {$token}"
+			),
+			CURLOPT_URL => "https://kapi.kakao.com/v1/api/story/delete/mystory", 
+			CURLOPT_SSL_VERIFYPEER => false, 
+				CURLOPT_SSLVERSION => 1,
+			CURLOPT_CUSTOMREQUEST => "DELETE",
+			CURLOPT_POSTFIELDS => $params, 
+			CURLOPT_RETURNTRANSFER => true, 
+		);
+		$curl = curl_init();
+		curl_setopt_array($curl, $opts);
+		$result = curl_exec($curl);
+		echo $result;
+		curl_close($curl);
+	}
 ?>
